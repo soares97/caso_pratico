1bf4494e2b11079e0bca6277ee9d78ff97e62a31
soares97 <michel971345g@gmail.com>
2021-06-04 09:55:04 +0000

    Melhorias

--- pages/imagens/[id].js
+++ pages/imagens/[id].js
@@ -1,4 +1,5 @@
-//Importes para utilizar os componentes "Router", "Layout" e "Title".
+//Importes para utilizar os componentes "Router", "Layout", "Link" e "Title".
+import Link from 'next/link';
 import {useRouter} from 'next/router';
 import Layout from '../../componentes/layout';
 import Title from '../../componentes/title';
@@ -11,6 +12,20 @@ export default function imagens() {
         <Layout>
             <Title>Detalhes da Imagem </Title>
             <p>ID da Imagem: {router.query.id}</p>
+            <Link href="/imagens">
+                <a>Voltar</a>
+            </Link>
+            <style jsx>
+                {`
+            a{
+                padding: 7px 12px;
+                text-decoration:none;
+                color: white;
+                background-color: #0070f3;
+                border-radius: 5px;
+            }
+            `}
+            </style>
         </Layout>
     )
 }
--- pages/postes/[id].js
+++ pages/postes/[id].js
@@ -1,4 +1,5 @@
-//Importes para utilizar os componentes "Router", "Layout" e "Title".
+//Importes para utilizar os componentes "Link", "Router", "Layout" e "Title".
+import Link from 'next/link';
 import {useRouter} from 'next/router';
 import Layout from '../../componentes/layout';
 import Title from '../../componentes/title';
@@ -11,6 +12,20 @@ export default function post() {
         <Layout>
             <Title>Detalhes do Poste </Title>
             <p>ID do Poste: {router.query.id}</p>
+            <Link href="/postes">
+                <a>Voltar</a>
+            </Link>
+            <style jsx>
+                {`
+            a{
+                padding: 7px 12px;
+                text-decoration:none;
+                color: white;
+                background-color: #0070f3;
+                border-radius: 5px;
+            }
+            `}
+            </style>
         </Layout>
     )
 }
